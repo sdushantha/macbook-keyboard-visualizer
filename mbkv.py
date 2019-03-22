99f22dc1e9ddc999a025477646d813918ca6b3a1
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2019-03-22 06:33:57 +0100

    cleaned up a little

--- mbkv.py
+++ mbkv.py
@@ -16,25 +16,24 @@ stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK)
 
 
-# Need to read the blog post about the audio recording with python and then I have to try
-# to try to understand the the algorithm on how line 15 to 18 works. It would also be inice if you could try to make
-# sensor a littl emore sensitive to the mic so that the the lights change more frquently and with more light difference :)
 while True:
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
     peak=np.average(np.abs(data))*2
-    #print(peak)
+    
     v = int(50*peak/2**16)
 
+    # Just printing the value of v so that I can make this script
+    # more accurate. 
     print(v)
     
     if v == 0:
         os.system("kbdlight set 0")
+    
     elif v < 8 and v > 0:
         os.system("kbdlight set 127")
 
     else:
         os.system("kbdlight max")
-    #print("%04d %05d %s"%(i,peak,bars))
 
 stream.stop_stream()
 stream.close()
